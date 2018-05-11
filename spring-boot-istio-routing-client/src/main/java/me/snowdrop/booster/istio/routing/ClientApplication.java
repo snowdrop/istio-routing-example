ba65d9f509d03d03bd4771535c89fbeb63931361
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2018-05-11 12:56:41 -0400

    Rename booster and apply naming standards

---
+++ spring-boot-istio-routing-client/src/main/java/me/snowdrop/booster/istio/routing/ClientApplication.java
@@ -0,0 +1,28 @@
+package me.snowdrop.booster.istio.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Controller
+public class ClientApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@GetMapping("")
+	public String index() {
+		return "index";
+	}
+
+}
