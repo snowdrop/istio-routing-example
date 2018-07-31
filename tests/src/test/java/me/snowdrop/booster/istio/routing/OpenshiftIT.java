3b2b45d87c64b2345c7adcaa135b75464a73f5ba
mocenas <mocenas@redhat.com>
2018-07-31 16:09:31 +0200

    updated integration tests to istio 0.8.0

--- tests/src/test/java/me/snowdrop/booster/istio/routing/OpenshiftIT.java
+++ tests/src/test/java/me/snowdrop/booster/istio/routing/OpenshiftIT.java
@@ -1,21 +1,20 @@
 package me.snowdrop.booster.istio.routing;
 
-import io.fabric8.openshift.api.model.v3_1.Route;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import me.snowdrop.istio.api.model.IstioResource;
 import org.apache.http.HttpStatus;
+import org.arquillian.cube.istio.api.IstioResource;
 import org.arquillian.cube.istio.impl.IstioAssistant;
-import org.arquillian.cube.openshift.impl.client.OpenShiftAssistant;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -23,55 +22,30 @@ import java.util.concurrent.TimeUnit;
 
 import static org.awaitility.Awaitility.await;
 
+/**
+ * @author Martin Ocenas
+ */
 @RunWith(Arquillian.class)
+@IstioResource("classpath:client-gateway-rule.yml")
 public class OpenshiftIT{
+    private static final String ISTIO_NAMESPACE = "istio-system";
+    private static final String ISTIO_INGRESS_GATEWAY_NAME = "istio-ingressgateway";
+
+    private static final int MEASURMENT_COUNT = 200;
+
+    @RouteURL(value = ISTIO_INGRESS_GATEWAY_NAME, namespace = ISTIO_NAMESPACE)
+    private URL ingressGatewayURL;
+
     @ArquillianResource
     private IstioAssistant istioAssistant;
-
-    @ArquillianResource
-    private OpenShiftAssistant openShiftAssistant;
-
-    private String istioURL;
 
     private final String appUrl = "example/";
     private final String dataUrlSuffix = "request-data";
 
-    private List<IstioResource> clientRouteRule = null;
-    private List<IstioResource> additionalRouteRule = null;
-
-    @Before
-    public void init() throws Exception {
-        // get istio ingress route
-        Route route = openShiftAssistant.getClient()
-                .routes()
-                .inNamespace("istio-system")
-                .withName("istio-ingress")
-                .get();
-        if (route == null){
-            throw new Exception("Istio ingress route not found");
-        }
-        istioURL = "http://" + route.getSpec().getHost() + "/";
-
-        clientRouteRule = deployRouteRule("client-route-rule.yml");
-        await()
-                .pollInterval(1, TimeUnit.SECONDS)
-                .atMost(1, TimeUnit.MINUTES)
-                .until(() -> {
-            try {
-                Response response = RestAssured.get(istioURL + appUrl);
-                return response.getStatusCode() == 200;
-            } catch (Exception ignored) {
-                return false;
-            }
-        });
-    }
+    private List<me.snowdrop.istio.api.model.IstioResource> additionalRouteRule = null;
 
     @After
     public void cleanup(){
-        if (clientRouteRule != null) {
-            istioAssistant.undeployIstioResources(clientRouteRule);
-            clientRouteRule = null;
-        }
         if (additionalRouteRule != null) {
             istioAssistant.undeployIstioResources(additionalRouteRule);
             additionalRouteRule = null;
@@ -80,26 +54,29 @@ public class OpenshiftIT{
 
     @Test
     public void basicAccessTest() {
+        waitUntilApplicationIsReady();
         RestAssured
                 .expect()
                 .statusCode(HttpStatus.SC_OK)
                 .when()
-                .get(istioURL + appUrl);
+                .get(ingressGatewayURL + appUrl);
     }
 
     @Test
     public void defaultLoadBalancingTest() throws Exception {
+        waitUntilApplicationIsReady();
         expectLoadBalancingRatio(50, 5);
     }
 
     @Test
     public void unequalLoadBalancingTest() throws Exception {
         additionalRouteRule = deployRouteRule("load-balancing-rule.yml");
+        waitUntilApplicationIsReady();
         Thread.sleep(10000); //sleep 10 sec to make rule take effect
         expectLoadBalancingRatio(80, 10);
     }
 
-    private List<IstioResource> deployRouteRule(String routeRuleFile) throws IOException {
+    private List<me.snowdrop.istio.api.model.IstioResource> deployRouteRule(String routeRuleFile) throws IOException {
         return istioAssistant.deployIstioResources(
                 Files.newInputStream(Paths.get("../rules/" + routeRuleFile)));
     }
@@ -126,7 +103,7 @@ public class OpenshiftIT{
         int serviceBCount = 0;
 
         int i;
-        for ( i = 0; i < 100 ; i++){
+        for ( i = 0; i < MEASURMENT_COUNT ; i++){
             Response response;
             int tries=0;
 
@@ -135,7 +112,7 @@ public class OpenshiftIT{
             do {
                 response = RestAssured
                     .when()
-                    .get(istioURL + appUrl + dataUrlSuffix);
+                    .get(ingressGatewayURL + appUrl + dataUrlSuffix);
                tries++;
             } while (response.statusCode() != 200 && tries <= 3);
 
@@ -151,7 +128,22 @@ public class OpenshiftIT{
                 serviceBCount++;
             }
         }
-        double requestsPercentRatio = i/100;
+        double requestsPercentRatio = i/100; // compute coefficient to recompute results to percents
         return Math.max((double)serviceACount / requestsPercentRatio, (double)serviceBCount / requestsPercentRatio);
+    }
+
+    private void waitUntilApplicationIsReady() {
+        await()
+                .pollInterval(1, TimeUnit.SECONDS)
+                .atMost(1, TimeUnit.MINUTES)
+                .untilAsserted(() ->
+                        RestAssured
+                                .given()
+                                .baseUri(ingressGatewayURL.toString())
+                                .when()
+                                .get(appUrl + dataUrlSuffix)
+                                .then()
+                                .statusCode(200)
+                );
     }
 }
